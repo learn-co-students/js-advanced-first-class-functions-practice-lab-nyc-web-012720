98002950a40fc7cd7b204fe44b1c7e8db6edc18b
haltonba <haltonba94@gmail.com>
2020-03-22 17:23:31 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solution in this file!
+function logDriverNames(drivers) {
+    drivers.forEach(driver => {
+        console.log(driver.name)
+    });
+}
+
+function logDriversByHometown(drivers, hometown) {
+    drivers.forEach(driver => {
+       if (driver.hometown === hometown) {
+           console.log(driver.name)
+       } 
+    });
+}
+
+function driversByRevenue(drivers) {
+    return drivers.slice().sort((driverOne, driverTwo) => {
+        return driverOne.revenue - driverTwo.revenue
+    }
+    )
+}
+
+function driversByName(drivers) {
+    return drivers.slice().sort((driverOne, driverTwo) => {
+        return driverOne.name.localeCompare(driverTwo.name)
+    })
+}
+
+function totalRevenue(drivers) {
+    return drivers.reduce((total, currentDriver) => {
+        return currentDriver.revenue + total
+    }, 0)
+}
+
+function averageRevenue(drivers) {
+    return totalRevenue(drivers) / drivers.length
+}
